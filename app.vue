3357318704d5fd3d4e443fddce90de41331a81ab
Leo <le4ndro.fioretti@gmail.com>
2024-12-06 15:02:54 +0100

    d

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <NuxtRouteAnnouncer />
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
 </template>
--- layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="w-screen h-screen">
-    <slot />
-  </div>
-</template>
-
-<script lang="ts" setup></script>
-
-<style></style>
